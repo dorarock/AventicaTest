4d855c044cb87335f8d5c56e2a1cffb55d6c816e
Andy Lee <xatariboyx@gmail.com>
2020-02-13 11:46:51 +0300

    исправлен баг с инпутом карточки раздела draft

--- src/Draft.js
+++ src/Draft.js
@@ -5,7 +5,7 @@ export default props => (
         <h2>Draft</h2>
         {  props.newCards.length > 0 ? props.newCards.map(newCard => (
             <div className="draft_card" key={newCard.id}>
-               <input className="changeTxtInput" type="text" placeholder="Enter text" data-id={newCard.id}></input>
+               <input className="changeTxtInput" type="text" placeholder={newCard.name} data-id={newCard.id}></input>
                 <span className="draft_card_txt" onClick={(evt) => props.changeText(newCard, evt)}> {newCard.name}</span>
                 <div className="btn_draft_wrapper">
                     <button className="draft_btn btn_remove crt_btn" data-id={newCard.id} onClick={(evt) => props.removeFromDraft(newCard)}>Remove</button>
